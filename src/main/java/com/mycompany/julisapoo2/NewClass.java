695c5d5281465342b92bffa765bbe3460bbc9e9c
Julisa650644 <gabymerin1996@gmail.com>
2025-06-13 19:42:37 -0500

    - Archivo de ejemplo

--- src/main/java/com/mycompany/julisapoo2/NewClass.java
+++ src/main/java/com/mycompany/julisapoo2/NewClass.java
@@ -1,17 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.julisapoo2;
 
 /**
  *
  * @author DELL
  */
-public class JulisaPOO2 {
-
-    public static void main(String[] args) {
-        System.out.println("HOLA MUNDO!");
-        
-    }
+public class NewClass {
+    
 }
