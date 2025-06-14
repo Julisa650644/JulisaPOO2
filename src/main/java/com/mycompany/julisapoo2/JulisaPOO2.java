159da311aa247ae01f2dfd35b7e307b4a1491c77
Julisa Merino <jg.merinob@uea.edu.ec>
2025-06-13 19:09:39 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/julisapoo2/JulisaPOO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.julisapoo2;
+
+/**
+ *
+ * @author DELL
+ */
+public class JulisaPOO2 {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!");
+    }
+}
